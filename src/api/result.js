6a6777be60208acdf7e8c5020b0840d42f0d15f1
Jeeann <jeeann.kim@gmail.com>
2023-01-20 01:33:38 +0900

    add result data file

---
+++ src/api/result.js
@@ -0,0 +1,48 @@
+let results = [
+  {
+    id: 0,
+    name: "Gryffindor",
+    traits: "Courage, bravery, nerve and chivalry",
+    founder: "Godric Gryffindor",
+    animal: "Lion",
+    colour: "Scarlet red and gold",
+    element: "Fire",
+    head: "Minerva Mcgonagall",
+    ghost: "Nearly headless Nick",
+  },
+  {
+    id: 1,
+    name: "Slytherin",
+    traits: "Ambition, cunning, leadership, and resourcefulness",
+    founder: "Salazar Slytherin",
+    animal: "Serpent",
+    colour: "Green and silver",
+    element: "Water",
+    head: "Severus Snape",
+    ghost: "Bloody Baron",
+  },
+  {
+    id: 2,
+    name: "Ravenclaw",
+    traits: "Intelligence, learning, wisdom and wit",
+    founder: "Rowena Ravenclaw",
+    animal: "Eagle",
+    colour: "Blue and bronze",
+    element: "Air",
+    head: "Filius Flitwick",
+    ghost: "Grey Lady",
+  },
+  {
+    id: 3,
+    name: "Hufflepuff",
+    traits: "Hard work, patience, justice, and loyalty",
+    founder: "Helga Hufflepuff",
+    animal: "Badger",
+    colour: "Canary yellow and black",
+    element: "Earth",
+    head: "Pomona Sprout",
+    ghost: "Fat Friar",
+  },
+];
+
+export default data;
