54cdd80f7f391ecf1f5237119838a007ca9c81f5
Daniela Küster <daniela.kuester@gmx.net>
2023-01-21 14:30:44 +0100

    Style: Add hover effect to button

--- src/container/LandingPage/LandingPage.jsx
+++ src/container/LandingPage/LandingPage.jsx
@@ -11,9 +11,8 @@ export default function LandingPage() {
     return(
         <div className="LandingPage__container">
             <h1>Hogwarts House Mysteries</h1>
-            <p>Which house would you belong to?</p>
-            <p>You know we can't all be in Ravenclaw, right? ;-)</p>
-            <button className="LandingPage__button" onClick={clickButton}>START THE QUIZ</button>
+            <p>Which house would you belong to? You know we can't all be in Ravenclaw, right? ;-)</p>
+            <button className="LandingPage__button LandingPage__buton-fade" onClick={clickButton}>START THE QUIZ</button>
         </div>
     );
 }
