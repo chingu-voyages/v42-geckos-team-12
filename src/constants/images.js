00771d8c2dac5fa03e539892a7a187604a2dde21
ellrub <ellefsen88@gmail.com>
2023-01-29 17:22:14 +0100

    refactor
    Added new images to import

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,10 +2,14 @@ import gryffindorSigil from "../assets/gryffindor-sigil.png";
 import hufflepuffSigil from "../assets/hufflepuff-sigil.png";
 import rawenclawSigil from "../assets/rawenclaw-sigil.png";
 import slytherinSigil from "../assets/slytherin-sigil.png";
+import sortingHat from "../assets/sorting-hat.png";
+import sparklers from "../assets/sparklers-small.png";
 
 export default {
     gryffindorSigil,
     hufflepuffSigil,
     rawenclawSigil,
-    slytherinSigil
+    slytherinSigil,
+    sortingHat,
+    sparklers
 };
