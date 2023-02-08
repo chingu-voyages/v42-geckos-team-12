0c9af7f9aa94fa14584725fe1317ae003316d511
Hitch95 <bahloul.condorcet@gmail.com>
2023-02-08 22:56:38 +0100

    responsive footer proposal one

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import { Routes, Route, Link } from "react-router-dom";
 import { LandingPage, Quiz, Footer } from "./container"
 import "./App.css";
 
+import { Gryffindor, Hufflepuff, LandingPage, Quiz, Ravenclaw, Result, Slytherin } from "./container"
+
+import "./App.css";
+
 
 function App() {
   return (
@@ -13,6 +17,11 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/quiz" element={<Quiz />}></Route>
+        <Route path="/gryffindor" element={<Gryffindor />}></Route>
+        <Route path="/slytherin" element={<Slytherin />}></Route>
+        <Route path="/ravenclaw" element={<Ravenclaw />}></Route>
+        <Route path="/hufflepuff" element={<Hufflepuff />}></Route>
+        <Route path="/result" element={<Result />}></Route>
       </Routes>
       <Footer />
     </div>
--- src/container/index.js
+++ src/container/index.js
@@ -1,3 +1,7 @@
 export { default as LandingPage } from "./LandingPage/LandingPage";
 export { default as Quiz } from "./Quiz/Quiz";
-export { default as Footer } from "./Footer/Footer";
+export { default as Gryffindor } from "./Gryffindor/Gryffindor";
+export { default as Slytherin } from "./Slytherin/Slytherin";
+export { default as Ravenclaw } from "./Ravenclaw/Ravenclaw";
+export { default as Hufflepuff } from "./Hufflepuff/Hufflepuff";
+export { default as Result } from "./Result/Result";
