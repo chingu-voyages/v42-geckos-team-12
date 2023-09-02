c6cf28194cd735d1a9cebec5dde9a2931b15575c
Jeeann <jeeann.kim@gmail.com>
2023-09-02 18:15:14 +0100

    edit footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React from 'react';
 
-import "./Footer.scss";
-
-
-export default function Footer () {
-    return (
-        <footer className="footer">
-            <div className="footer__container">
-                <p className="footer__copyright">Copyright 2023</p>
-                <img
-                    className="footer__img-hp"
-                    src="https://user-images.githubusercontent.com/74983536/128005620-f7f44ba5-14d7-4329-abe4-0da9c93137e1.png" 
-                    alt="Harry Potter Logo" />
-            </div>
-            <div className="footer__container">
-                <a href="https://www.chingu.io/" target="_blank" rel="noreferrer noopener">
-                    <img className="footer__img-chingu" src="https://www.chingu.io/logo-with-text-192.png" alt="Chingu logo" />
-                </a>
-                <a href="https://github.com/chingu-voyages/v42-geckos-team-12" target="_blank" rel="noreferrer noopener">
-                    <img className="footer__img-github" src="https://avatars.githubusercontent.com/u/583231?v=4" alt="GitHub logo" />
-                </a>
-            </div>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer style={{ position: 'fixed', left: 0, bottom: 0, width: '100%' }}>
+      <div>
+        <a href="https://github.com/chingu-voyages/v42-geckos-team-12">
+          <svg
+            width="2rem"
+            height="2rem"
+            viewBox="0 0 16 16"
+            version="1.1"
+            data-view-component="true"
+            transform="translateY(1%)"
+            fill="#8a5e44"
+          >
+            <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+          </svg>
+        </a>
+      </div>
+      <p
+        style={{
+          fontFamily: 'Baskervville, serif',
+          padding: 0,
+          marginTop: 9,
+        }}
+      >
+        Copyright © 2023 Chingu v42 geckos 12. All rights reserved.
+      </p>
+    </footer>
+  );
 }
